a835c276fa8823325cf037cb194b7a6121b2a4c5
Esity <matthewdiverson@gmail.com>
2026-03-18 07:03:51 -0500

    fix version spec to match 0.2.1

--- spec/legion/extensions/cortex_spec.rb
+++ spec/legion/extensions/cortex_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe Legion::Extensions::Cortex do
   end
 
   it 'has a version string' do
-    expect(Legion::Extensions::Cortex::VERSION).to eq('0.2.0')
+    expect(Legion::Extensions::Cortex::VERSION).to eq('0.2.1')
   end
 end
